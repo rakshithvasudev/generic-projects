8837435b911569b677f6b5eb7246428b71a4a871
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-09-08 18:21:55 -0700

    adding comments

--- Chuckle-HW1/src/Game.java
+++ Chuckle-HW1/src/Game.java
@@ -6,17 +6,15 @@ import java.util.Map;
  * Assignment. This class contains the entire chuckle game
  * elements including the dice rolls, chosen values aka the "scoreboard", that
  * tracks the number of times a roll event occurred.
- *
- *
  */
 public class Game {
     //chosenValue tracks the values as follows:
     //{score:number of dice rolls},
     // example :{2:0,3:1,4:2......12:4}, Here 2: is rolled 0 times, while 12 was rolled 4 times.}
-    private Map<Integer,Integer> chosenValues;
-    private Die die1,die2;
+    private Map<Integer, Integer> chosenValues;
+    private Die die1, die2;
 
-    public Game(){
+    public Game() {
         die1 = new Die();
         die2 = new Die();
         chosenValues = new HashMap<>();
@@ -25,22 +23,38 @@ public class Game {
 
     /**
      * Starts the game by initializing the scoreboard - chosenvalues.
-     * 
-     *
      */
     private void initializeGame() {
-    for (int i=2;i<12;i++)
-            chosenValues.put(i,0);
+        for (int i = 2; i < 12; i++)
+            chosenValues.put(i, 0);
     }
 
     /**
      * Rolls both the dice and
      */
-    private void rollDice(){
-     int die1Value = die1.roll(),die2Value = die2.roll();
+    private void rollDice() {
+        int die1Value = die1.roll(), die2Value = die2.roll();
+        System.out.println("The rolled dice values are : (" + die1Value + ", " + die2Value + ") => " + die1Value + die2Value);
+        updateScoreBoard(die1Value + die2Value);
     }
 
+    /**
+     * Updates the scoreboard map. Adds 1 to the value of the map of the key "sumOfRolledDice".
+     * Before updating the value to the scoreboard, the  
+     * @param sumOfRolledDice key to update the scoreboard.
+     */
+    private void updateScoreBoard(int sumOfRolledDice) {
+        checkForFullScore();
+        try{
+            chosenValues.put(sumOfRolledDice,chosenValues.get(sumOfRolledDice)+1);
+        }catch (NullPointerException e){
+            System.out.println("Error occurred please replay the game.");
+        }
+    }
 
+    private void checkForFullScore() {
+
+    }
 
 
 }
