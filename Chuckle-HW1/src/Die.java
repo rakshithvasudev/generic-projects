09e11fb9ed7854195e069bcfaee94b0877749be4
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-09-08 18:06:46 -0700

    adding comments

--- Chuckle-HW1/src/Die.java
+++ Chuckle-HW1/src/Die.java
@@ -5,34 +5,50 @@ import java.util.concurrent.ThreadLocalRandom;
  */
 public class Die {
     private int faceValue;
-    public int roll(){
+
+    /**
+     * This function simulates the roll event that can return any random
+     * value between 1 and 6, much like an actual dice.
+     *
+     * @return a random value between 1 and 6 using ThreadLocalRandom.
+     */
+    public int roll() {
         faceValue = ThreadLocalRandom.current().nextInt(1, 6 + 1);
         return faceValue;
     }
 
     @Override
     public String toString() {
-        return ("["+faceValue+":]");
+        return ("[" + faceValue + ":]");
     }
 
     /**
-     * This function overrides the default 
+     * Helps to support unique value identification.
+     * This function overrides the default hashcode from java
+     * to prevent in a violation of the general contract for
+     * Object.hashCode(), which will prevent the class from functioning properly
+     * in conjunction with all hash-based collections, including HashMap, HashSet, and Hashtable.
+     *
      * @return the hashed value of the Die.
      */
     @Override
     public int hashCode() {
         int a = 883;
-        a = faceValue*a*1033;
-        a += faceValue*a;
+        a = faceValue * a * 1033;
+        a += faceValue * a;
         return a;
     }
 
+    /**
+     * @param obj Object for checking for equality in input.
+     * @return True only if the object is equal in value and not via reference.
+     */
     @Override
     public boolean equals(Object obj) {
-        if(obj!=null&& this.getClass()==obj.getClass()){
-            Die objEqual = (Die)obj;
+        if (obj != null && this.getClass() == obj.getClass()) {
+            Die objEqual = (Die) obj;
             return objEqual.faceValue == this.faceValue;
         }
-    return false;
+        return false;
     }
 }
--- Chuckle-HW1/src/Game.java
+++ Chuckle-HW1/src/Game.java
@@ -4,31 +4,40 @@ import java.util.Map;
 /**
  * Created by Rakshith on 9/7/2017 for CSC527 - Software Construction
  * Assignment. This class contains the entire chuckle game
- * elements including the dice rolls, chosen values aka the scoreboard,
+ * elements including the dice rolls, chosen values aka the "scoreboard", that
+ * tracks the number of times a roll event occurred.
  *
  *
  */
 public class Game {
-    private static int rollsCounter;
+    //chosenValue tracks the values as follows:
+    //{score:number of dice rolls},
+    // example :{2:0,3:1,4:2......12:4}, Here 2: is rolled 0 times, while 12 was rolled 4 times.}
     private Map<Integer,Integer> chosenValues;
     private Die die1,die2;
 
     public Game(){
-        rollsCounter=0;
         die1 = new Die();
         die2 = new Die();
         chosenValues = new HashMap<>();
-        initalizeGame();
+        initializeGame();
     }
 
-    private void initalizeGame() {
+    /**
+     * Starts the game by initializing the scoreboard - chosenvalues.
+     * 
+     *
+     */
+    private void initializeGame() {
     for (int i=2;i<12;i++)
             chosenValues.put(i,0);
     }
 
+    /**
+     * Rolls both the dice and
+     */
     private void rollDice(){
      int die1Value = die1.roll(),die2Value = die2.roll();
-     rollsCounter++;
     }
 
 
