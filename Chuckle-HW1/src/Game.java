9cf13bf655b6ceda09d2bda7831126feef70944f
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-09-08 19:18:17 -0700

    finishing steps

--- Chuckle-HW1/src/Game.java
+++ Chuckle-HW1/src/Game.java
@@ -25,16 +25,17 @@ public class Game {
      * Starts the game by initializing the scoreboard - chosenvalues.
      */
     private void initializeGame() {
-        for (int i = 2; i < 12; i++)
+        for (int i = 2; i <= 12; i++)
             chosenValues.put(i, 0);
     }
 
     /**
      * Rolls both the dice and
      */
-    private void rollDice() {
+    public void rollDice() {
         int die1Value = die1.roll(), die2Value = die2.roll();
-        System.out.println("The rolled dice values are : (" + die1Value + ", " + die2Value + ") => " + die1Value + die2Value);
+        int sumOfDice = die1Value+die2Value;
+        System.out.println("The rolled dice values are : (" + die1Value + ", " + die2Value + ") => "+ sumOfDice);
         updateScoreBoard(die1Value + die2Value);
     }
 
@@ -42,20 +43,42 @@ public class Game {
      * Updates the scoreboard map. Adds 1 to the value of the map of the key "sumOfRolledDice".
      * Before updating the value to the scoreboard, the function checks if already the scoreboard is full
      * atleast once. If it's full, the game stops otherwise it adds to the scoreboard.
+     *
      * @param sumOfRolledDice key to update the scoreboard.
      */
     private void updateScoreBoard(int sumOfRolledDice) {
-        checkForFullScore();
-        try{
-            chosenValues.put(sumOfRolledDice,chosenValues.get(sumOfRolledDice)+1);
-        }catch (NullPointerException e){
-            System.out.println("Error occurred please replay the game.");
+        if (!checkForFullScore()) {
+            try {
+                chosenValues.put(sumOfRolledDice, chosenValues.get(sumOfRolledDice) + 1);
+            } catch (NullPointerException e) {
+                System.out.println("Error occurred please replay the game.\n");
+            }
+        } else {
+            System.out.println("Congrats, you've finished the game with :" + findTotalRolls() + " rolls.\n \n");
+            System.out.println("Here is your final scoreboard :\n ");
+            displayScoreBoard();
+            System.exit(0);
         }
+    }
+
+    /**
+     * Finds the total number of rolls in the values of the map.
+     * Experimenting with java 8 lambdas seem to be more efficient
+     * than regular for each loop as discussed here :
+     * https://stackoverflow.com/a/35558955
+     *
+     * @return the sum of values in the chosenvalues map.
+     */
+    private int findTotalRolls() {
+        final int[] rollsSum = {0};
+        chosenValues.forEach((k, v) -> rollsSum[0] += v);
+        return rollsSum[0];
     }
 
     /**
      * Checks if the scoreboard is already full atleast once. Iterates through the values and even if one
      * value is '0', then the method returns false.
+     *
      * @return True if full, False otherwise.
      */
     private boolean checkForFullScore() {
@@ -67,5 +90,8 @@ public class Game {
         return true;
     }
 
+    public void displayScoreBoard(){
+        System.out.println("Current Scoreboard is as follows: "+ chosenValues +"\n \n");
+    }
 
 }
--- Chuckle-HW1/src/Main.java
+++ Chuckle-HW1/src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Main class where the game is run.
+ */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Game game = new Game();
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Welcome to playing Chuckles. Here are your options:");
+
+        /**
+         * Keeps looping forever either until the user quits the game or user finishes the game.
+         */
+        while (true){
+            System.out.println("1. Roll Dice\n2. View scoreboard\n3. Exit Game: \n");
+            int option = reader.nextInt();
+            if(option==1) game.rollDice();
+            else if (option ==2) game.displayScoreBoard();
+            else if (option == 3) System.exit(0);
+        }
     }
 }
