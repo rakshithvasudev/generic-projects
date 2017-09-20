66f4178f34c5e699bd59c3b196d7e862f0b4c626
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-09-19 23:06:47 -0700

    added HW2

---
+++ Generics-HW2/src/Main.java
@@ -0,0 +1,10 @@
+import java.sql.Timestamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Timestamp ts = new Timestamp(System.currentTimeMillis());
+        System.out.println(ts);
+    }
+
+}
--- Generics-HW2/src/RegisteredVar.java
+++ Generics-HW2/src/RegisteredVar.java
@@ -1,3 +1,25 @@
-public class RegisteredVar {
+import java.sql.Timestamp;
+import java.util.List;
+
+public class RegisteredVar<T> {
+
+    private T genericData;
+    private int timesChanged;
+    private List<Timestamp> latestTimeStamp;
+    private List<RegisteredVar> lastThreeValues;
+    private int maxValue;
+    private int minValue;
+
+    public RegisteredVar(){
+        genericData = null;
+    }
+
+    public void setValue(T newValue){
+        if(genericData==null)
+
+            genericData = newValue;
+
+    }
+
 
 }
