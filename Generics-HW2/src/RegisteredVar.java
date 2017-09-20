024d4d417f3c92c6ba33f4fcaaa398c5805150e7
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-09-19 23:13:25 -0700

    added HW2

--- Generics-HW2/src/RegisteredVar.java
+++ Generics-HW2/src/RegisteredVar.java
@@ -1,24 +1,41 @@
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class demonstrates Generics in Java with updates.
+ * @param <T> Anydata type that user potentially wants to consider
+ *           passing.
+ */
 public class RegisteredVar<T> {
 
     private T genericData;
     private int timesChanged;
     private List<Timestamp> latestTimeStamp;
-    private List<RegisteredVar> lastThreeValues;
+    private List<T> lastThreeValues;
     private int maxValue;
     private int minValue;
 
     public RegisteredVar(){
         genericData = null;
+        timesChanged=0;
+        latestTimeStamp = new ArrayList<>();
+        lastThreeValues = new ArrayList<>();
+        maxValue=Integer.MIN_VALUE;
+        minValue=Integer.MAX_VALUE;
     }
 
+
     public void setValue(T newValue){
+        updateCounters();
+        genericData = newValue;
+    }
+
+    private void updateCounters() {
         if(genericData==null)
-
-            genericData = newValue;
-
+            timesChanged=0;
+        else
+            timesChanged+=1;
     }
 
 
