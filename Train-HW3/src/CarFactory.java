40bae5140eb25b6a6b94fa4fd553c52f8ed937b2
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-10-10 10:52:34 -0700

    added car factory

--- Train-HW3/src/CarFactory.java
+++ Train-HW3/src/CarFactory.java
@@ -5,8 +5,21 @@
  */
 public class CarFactory {
 
+    /**
+     * This method matches the name of the type of the car and generates a new car
+     * based on the type matched.
+     * @param type type of the car class to resolve to and construct.
+     * @param name name of the car.
+     * @param length length of the car.
+     * @param height height of the car.
+     * @param weight weight of the car.
+     * @param options options of the car.
+     * @return
+     */
     public static Car GenerateCar(String type,String name, int length, int height, int weight, String[] options){
-        Car newCar = null;
+
+        // newCar is a dummy variable that never should get returned.
+        Car newCar = new Car(name, length,  height,  weight, options);
 
         if(type.equalsIgnoreCase("Cargo")) {
             return new CargoCar(name, length, height, weight, options);
