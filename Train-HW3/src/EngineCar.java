a07fdc8f849f26846c013da54285e65d44db6873
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-10-09 22:51:24 -0700

    added cars subclasses

---
+++ Train-HW3/src/EngineCar.java
@@ -0,0 +1,6 @@
+public class EngineCar extends Car {
+
+    public EngineCar(String name, int length, int height, int weight, String[] options) {
+        super(name, length, height, weight, options);
+    }
+}
--- Train-HW3/src/RefrigeratedCar.java
+++ Train-HW3/src/RefrigeratedCar.java
@@ -1,5 +1,17 @@
+/**
+ * Created by Rakshith on oct 9 2017, for CSC527 - HW3.
+ * This car is a concrete class that gets created based on the type.
+ */
 public class RefrigeratedCar extends Car {
-
+    /**
+     * Creates a RefrigeratedCar from the car blueprint.
+     *
+     * @param name name of the car.
+     * @param length length of car.
+     * @param height height of car.
+     * @param weight weight of car.
+     * @param options options in the car.
+     */
     public RefrigeratedCar(String name, int length, int height, int weight, String[] options) {
         super(name, length, height, weight, options);
     }
