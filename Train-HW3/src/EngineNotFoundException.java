23d00d9b7fc4ce52df7c3f1936e351425c22e631
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-10-10 10:34:21 -0700

    added engine not found

--- Train-HW3/src/EngineNotFoundException.java
+++ Train-HW3/src/EngineNotFoundException.java
@@ -1,4 +1,15 @@
+/**
+ * Created by Rakshith on oct 10 2017, for CSC527 - HW3.
+ * This class is a custom exception that gets thrown when there's
+ * no engineCar in a given train class.
+ */
 public class EngineNotFoundException extends Exception {
 
+    /**
+     * @param message that needs to be thrown upon calling.
+     */
+    public  EngineNotFoundException(String message){
+        super(message);
+    }
 
 }
