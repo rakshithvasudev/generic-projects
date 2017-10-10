f73d3d08bd55a78f9c0eb0bc850436a442cb59d0
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-10-09 22:56:48 -0700

    added cars subclasses

--- Train-HW3/src/TankerCar.java
+++ Train-HW3/src/TankerCar.java
@@ -2,18 +2,17 @@
  * Created by Rakshith on oct 9 2017, for CSC527 - HW3.
  * This car is a concrete class that gets created based on the type.
  */
-public class EngineCar extends Car {
-
+public class TankerCar extends Car {
     /**
-     * Creates a EngineCar from the car blueprint.
+     * Creates a TankerCar from the car blueprint.
      *
      * @param name name of the car.
      * @param length length of car.
      * @param height height of car.
      * @param weight weight of car.
-     * @param options options in the car.
+     * @param options options in the car
      */
-    public EngineCar(String name, int length, int height, int weight, String[] options) {
+    public TankerCar(String name, int length, int height, int weight, String[] options) {
         super(name, length, height, weight, options);
     }
 }
