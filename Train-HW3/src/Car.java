4ae1b69b6d201c6179f59fc0f9487350517cad7d
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-10-09 23:44:43 -0700

    fixed tostring

--- Train-HW3/src/Car.java
+++ Train-HW3/src/Car.java
@@ -9,6 +9,14 @@ public class Car {
     protected int weight;
     protected String[] options;
 
+    /**
+     * Creates a car with specified parameters.
+     * @param name name of the car.
+     * @param length length of the car.
+     * @param height height of the car.
+     * @param weight weight of the car.
+     * @param options options of the car.
+     */
     public Car(String name, int length, int height, int weight, String[] options) {
         setName(name);
         setLength(length);
@@ -57,10 +65,19 @@ public class Car {
         this.options = options;
     }
 
+    /**
+     * This method ensures that all the inherited classes stick to the required representation
+     * as indicated by the requirement specs.
+     * example: CarType Name L*H : l x h  Wt: wt
+     * @return expected text representation of the car as indicated in the example.
+     */
     @Override
     public String toString() {
+
         String carType =  getClass().toString().replace("class ","" ).
                 replace("Car","");
-        return carType + " " + getName() + " L*H : "+ getLength()+ " x " +getHeight() + " " +" Wt: " +getWeight();
+        
+        return carType + " " + getName() + " L*H : "+ getLength()+
+                " x " +getHeight() + " " +" Wt: " +getWeight();
     }
 }
