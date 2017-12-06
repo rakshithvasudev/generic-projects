9dd43adb7c4dd668d63cded7612e469f8cd33809
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-12-06 00:47:26 -0800

    added extra credits

--- Extracredit/src/Main.java
+++ Extracredit/src/Main.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
@@ -8,19 +10,34 @@ public class Main {
 
 
     public static void main(String[] args) {
-
-        for (int i = 0; i < 300; i++) {
-            int randX = randomNumberGenerator(0, 600);
-            int randY = randomNumberGenerator(0, 400);
-            graphics.fillRect(randX, randY, 4, 4);
+        while(true){
+            generatePoints();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            drawingPanel.clear();
         }
+
     }
 
     public static int randomNumberGenerator(int min, int max) {
         return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
 
-    public static void generatePoints(int numberOfPoints) {
+    public static void generatePoints() {
 
+        List<Integer> randXPoints = new ArrayList<>();
+        List<Integer> randYPoints = new ArrayList<>();
+
+
+        for (int i = 0; i < 500; i++) {
+            int randX = randomNumberGenerator(0, 600);
+            int randY = randomNumberGenerator(0, 400);
+            graphics.fillRect(randX, randY, 4, 4);
+            randXPoints.add(randX);
+            randYPoints.add(randY);
+        }
     }
 }
