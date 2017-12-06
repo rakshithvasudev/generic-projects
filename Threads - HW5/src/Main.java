4a5a4ee1f747812a30c0c43d9598310a1380e3c2
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-12-05 19:00:30 -0800

    rndom thread inculcating

--- Threads - HW5/src/Main.java
+++ Threads - HW5/src/Main.java
@@ -3,17 +3,16 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Main implements Runnable{
 
     // used for choosing thread picking
-    private static int randomThreadNumber = 0;
-    private Circle c = new Circle(45,45,10);
-
+    private static int randomThreadNumber = randomNumberGenerator(1,4);
+    private static int xloc = 300, yloc =200, radius = 10;
+    private static Circle c = new Circle(xloc,yloc,radius);
 
     public static void main(String[] args) {
-
-
-
-
-
-
+        Main main = new Main();
+        new Thread(main).start();
+        new Thread(main).start();
+        new Thread(main).start();
+        new Thread(main).start();
     }
 
     @Override
@@ -21,22 +20,22 @@ public class Main implements Runnable{
 
         // left moving thread
         if(randomThreadNumber == 1){
-            c.moveCircleTo();
+            c.moveCircleBy(-5,0);
         }
 
         // right moving thread
         else if (randomThreadNumber == 2){
-
+           c.moveCircleBy(5,0);
         }
 
-        //top moving thread
+        //top moving thread (0 is at the top)
         else if(randomThreadNumber == 3){
-
+          c.moveCircleBy(0,-5);
         }
 
-        //bottom moving thread
+        //bottom moving thread (max y is at the bottom)
         else{
-
+          c.moveCircleBy(0,5);
         }
 
       randomThreadNumber = randomNumberGenerator(1,4);
