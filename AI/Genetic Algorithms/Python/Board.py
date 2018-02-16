93bf05bc0f7f4fe0a2b60d4a5c8ae4ae08f4f679
Rakshith Vasudev <rakshithvbharani@gmail.com>
2018-02-15 18:13:02 -0800

    adding board

--- AI/Genetic Algorithms/Python/Board.py
+++ AI/Genetic Algorithms/Python/Board.py
@@ -1,6 +1,17 @@
+# using numpy for easy array operations
 import numpy as np
 
-# generate a board having numbers from 1 to 9 whose
-# dimension is 9 x 9
-board = np.random.random_integers(1, 9, (9, 9))
-print(board)
+
+class SudokuBoard:
+    def __init__(self):
+        self.board_size = 9
+        self.generate_board()
+
+    def fitness_val(self):
+        return 100
+
+    def generate_board(self):
+        # generate a board having numbers from 1 to 9 whose
+        # dimension is 9 x 9
+        board = np.random.random_integers(1, 9, (9, 9))
+        print(board)
