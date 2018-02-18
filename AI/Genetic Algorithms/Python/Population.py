b8b68b5b9881ff9c0751cd5c139e9490c5f727d9
Rakshith Vasudev <rakshithvbharani@gmail.com>
2018-02-18 15:01:24 -0800

    added population solutions

--- AI/Genetic Algorithms/Python/Population.py
+++ AI/Genetic Algorithms/Python/Population.py
@@ -27,9 +27,9 @@ class Population:
         return self.population
 
     def generate_new_population(self):
-        self.breed(0.5, random.randint(1, 9), random.randint(1, 9))
+        self.breed(random.randint(1, 9), random.randint(1, 9))
 
-    def breed(self, probability=0.5, row_index=5, col_index=5):
+    def breed(self, row_index=5, col_index=5):
         if row_index > 9 or col_index > 9:
             raise ValueError("Indices can't be larger than board size")
 
@@ -40,16 +40,15 @@ class Population:
         for counting_index in range(stopping_point):
             chromosome1 = self.population[counting_index]
             chromosome2 = self.population[(max_limit - 1) - counting_index]
-            crossover_result = self.crossover(chromosome1, chromosome2, probability, row_index, col_index)
+            crossover_result = self.crossover(chromosome1, chromosome2, row_index, col_index)
             self.new_population.append(crossover_result[0])
             self.new_population.append(crossover_result[1])
 
-    def crossover(self, chromosome1, chromosome2, probability, row_index, col_index):
+    def crossover(self, chromosome1, chromosome2, row_index, col_index):
         """
         Performs crossover.
         :param chromosome1: parent 1
         :param chromosome2: parent 2
-        :param probability:
         :param row_index:
         :param col_index:
         :return:
