a2c375fa7677906116afc1cf15fdcacec749ffe9
Rakshith Vasudev <rakshithvbharani@gmail.com>
2018-02-18 14:55:01 -0800

    added breeding

--- AI/Genetic Algorithms/Python/Population.py
+++ AI/Genetic Algorithms/Python/Population.py
@@ -1,3 +1,6 @@
+import math
+import random
+
 from Board import SudokuBoard
 
 
@@ -5,6 +8,7 @@ class Population:
     def __init__(self, population_count=100):
         self.population = []
         self.population_count = population_count
+        self.new_population = []
 
     def initialize_populations(self):
         """
@@ -16,8 +20,49 @@ class Population:
             generated_board_object.generate_board()
             self.population.append(generated_board_object)
 
-    def sort_population(self):
+    def sort_original_population(self):
         self.population = sorted(self.population, key=lambda x: x.board_fitness_score(), reverse=True)
 
-    def get_boards(self):
+    def get_population(self):
         return self.population
+
+    def generate_new_population(self):
+        self.breed(0.5, random.randint(1, 9), random.randint(1, 9))
+
+    def breed(self, probability=0.5, row_index=5, col_index=5):
+        if row_index > 9 or col_index > 9:
+            raise ValueError("Indices can't be larger than board size")
+
+        max_limit = len(self.population)
+        stopping_point = int(math.ceil(max_limit / 2))
+
+        # get the parents from the end points and breed them
+        for counting_index in range(stopping_point):
+            chromosome1 = self.population[counting_index]
+            chromosome2 = self.population[(max_limit - 1) - counting_index]
+            crossover_result = self.crossover(chromosome1, chromosome2, probability, row_index, col_index)
+            self.new_population.append(crossover_result[0])
+            self.new_population.append(crossover_result[1])
+
+    def crossover(self, chromosome1, chromosome2, probability, row_index, col_index):
+        """
+        Performs crossover.
+        :param chromosome1: parent 1
+        :param chromosome2: parent 2
+        :param probability:
+        :param row_index:
+        :param col_index:
+        :return:
+        """
+
+        # here is where the cols and rows get swapped, thanks to numpy which makes this process
+        # painless.
+        chromosome1.get_board()[:row_index, :col_index] = chromosome2.get_board()[:row_index, :col_index]
+        chromosome2.get_board()[:row_index, :col_index] = chromosome1.get_board()[:row_index, :col_index]
+        return [chromosome1, chromosome2]
+
+    def sort_new_population(self):
+        self.new_population = sorted(self.new_population, key=lambda x: x.board_fitness_score(), reverse=True)
+
+    def get_new_population(self):
+        return self.new_population
--- AI/Genetic Algorithms/Python/playground.py
+++ AI/Genetic Algorithms/Python/playground.py
@@ -1,19 +1,38 @@
 from Population import Population
 from Board import SudokuBoard
-import time
+import time, numpy as np
 
 initial_time = time.time()
 pop = Population(1000000)
 pop.initialize_populations()
-pop.sort_population()
+pop.sort_original_population()
+old_pop = pop.get_population()
 
-boards = pop.get_boards()
+for new_board in old_pop:
+    print(new_board.board_fitness_score())
 
-for board in boards:
-    print(board.board_fitness_score())
+print("Original population: " + str(len(old_pop)))
+print("========================")
+print("breeding:....")
+pop.generate_new_population()
+pop.sort_new_population()
+new_pop = pop.get_new_population()
+
+for new_board in new_pop:
+    print(new_board.board_fitness_score())
 
 final_time = time.time()
-
+print("new population: " + str(len(new_pop)))
+print("Old population highest fitness score: " + str(old_pop[0].board_fitness_score()))
+print("New population highest fitness score: " + str(new_pop[0].board_fitness_score()))
 print("===================")
-print("Time to execute: {} s".format(final_time-initial_time))
+print("Time to execute: {} s".format(final_time - initial_time))
 
+# a = np.array(np.random.random_integers(1, 10, (9, 9)))
+# b = np.array(np.random.random_integers(1, 10, (9, 9)))
+# print(a)
+# print("=============================")
+# print(b)
+# a[:3,] = b[:3,]
+# print("=============================")
+# print(a)
