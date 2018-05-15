19545f31a76069af8aee2636522b757f83766599
Rakshith Vasudev <rakshithvbharani@gmail.com>
2018-05-15 13:40:58 -0700

    adding probabilities

--- AI/Markov Process Java/src/Action.java
+++ AI/Markov Process Java/src/Action.java
@@ -4,16 +4,27 @@ import java.util.List;
 public class Action {
 
     public static void main(String[] args) {
-//          String sample = "John's tire was not 12 inches across anymore!";
-        Markov mvp = new Markov();
-        String sample = mvp.readText("bible.txt");
-        List<Character> lists = mvp.convertToGroups(sample);
+        Markov mvp1 = new Markov();
+        String sample = mvp1.readText("bible.txt");
+        List<Character> lists = mvp1.convertToGroups(sample);
         String groupedString = lists.toString().replace(", ","");
-//        System.out.println(groupedString);
-        mvp.populateFirstMatrix(groupedString);
-        mvp.populateSecondMatrix(groupedString);
-        System.out.println(Arrays.deepToString(mvp.calculateProbabilites(mvp.firstTransitionMatrix)));
-        System.out.println(Arrays.deepToString(mvp.calculateProbabilites(mvp.secondTransitionMatrix)));
+        mvp1.populateFirstMatrix(groupedString);
+        mvp1.populateSecondMatrix(groupedString);
+        System.out.println(Arrays.deepToString(mvp1.calculateProbabilites(mvp1.firstTransitionMatrix)));
+        System.out.println(Arrays.deepToString(mvp1.calculateProbabilites(mvp1.secondTransitionMatrix)));
+        System.out.println("Not a 1st order ");
+
+        System.out.println("-------------------------------------------------------------------------");
+
+        Markov mvp2 = new Markov();
+        String sample2 = mvp2.readText("alger.txt");
+        List<Character> lists2 = mvp2.convertToGroups(sample2);
+        String groupedString2 = lists2.toString().replace(", ","");
+        mvp2.populateFirstMatrix(groupedString2);
+        mvp2.populateSecondMatrix(groupedString2);
+        System.out.println(Arrays.deepToString(mvp2.calculateProbabilites(mvp2.firstTransitionMatrix)));
+        System.out.println(Arrays.deepToString(mvp2.calculateProbabilites(mvp2.secondTransitionMatrix)));
+        System.out.println("Not a 1st order ");
     }
 
 }
